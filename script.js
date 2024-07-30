9de989e1f711f49d6cc908160c40299b763125dc
dsordes <dsordesdinho34@gmail.com>
2024-07-29 23:15:49 -0300

    versão 1 completa

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ const nome=document.getElementById('nome')
 const valor=document.getElementById('valor_uni')
 const quantidade=document.getElementById('quantidade')
 
+const up_id=document.getElementById('up_id')
+const up_coluna=document.getElementById('up_coluna')
+const up_valor=document.getElementById('up_valor')
+const del_id=document.getElementById('del_id')
+
+
+
 const bt_busca=document.getElementById('busca')
 const bt_limpa=document.getElementById('limpa')
 const bt_lista=document.getElementById('lista')
 const bt_insere=document.getElementById('insere')
+const bt_edita=document.getElementById('edita')
+const bt_deleta=document.getElementById('deleta')
 
 const tabela=document.getElementById('tabela')
 
 const aviso=document.getElementById('aviso')
 
+
+
+//FUNCTIONS DE ACESSO=================================
 async function getVenda(ida){
     const url=`https://fastapi-ds.up.railway.app/pesquisa?id=${ida}`
     //const url=`http://localhost:8000/pesquisa?id=${ida}`
@@ -53,8 +65,35 @@ async function insertVendas(nome, valor, quantidade){
 }
 
 
+async function editaVendas(id, coluna, valor){
+    const url=`https://fastapi-ds.up.railway.app/update/${id}`
+    //const url=`http://localhost:8000/update/${id}`
+
+    if(id!=''&&coluna!=''&&valor!=''){
+        fetch(url, {
+            method:"PUT",
+            body: JSON.stringify({
+                coluna:coluna,
+                valor:valor
+            }), headers:{
+                "Content-type":"application/json;charset=UTF-8"
+            }
+        }).then((response)=>response.json()).then((element)=>{console.log(element)})
+    }
+}
+
+async function deletaVenda(id){
+    const url=`https://fastapi-ds.up.railway.app/delete/${id}`
+    //const url=`http://localhost:8000/delete/${id}`
+
+    fetch(url, {
+        method:"DELETE"
+    }).then((response)=>response.json()).then((element)=>{console.log(element)})
+
+}
 
 
+//FUNCTIONS DE CONTROLE =================================
 
 function limpar(){
     tabela.innerHTML=''
@@ -71,14 +110,14 @@ function criaLinha(venda_info){
     }else{
 
         let vid=venda_info.id
-        let item=venda_info.item
+        let nome=venda_info.nome
         let preco=venda_info.preco_uni
         let qtd=venda_info.quantidade
 
         tabela.innerHTML+=`
             <tr>
                 <td>${vid}</td>
-                <td>${item}</td>
+                <td>${nome}</td>
                 <td>${preco}</td>
                 <td>${qtd}</td>
             </tr>
@@ -94,14 +133,17 @@ function criaTabela(vendas){
     });
 }
 
+//LISTENERS ========================================
+
 
 bt_busca.addEventListener('click', ()=>{
     if(id.value==''){
         aviso.innerText='Você não escreveu um id.'
     }else{
-        tabela.innerHTML=''
-        aviso.innerText=''
-        getVenda(id.value)
+        venda_id=id.value
+        limpar()
+        
+        getVenda(venda_id)
     }
 })
 
@@ -119,5 +161,23 @@ bt_insere.addEventListener('click', ()=>{
     nome.value= ''
     valor.value= ''
     quantidade.value= ''
+    getVendas()
+})
+
+
+bt_edita.addEventListener('click', ()=>{
+    editaVendas(up_id.value, up_coluna.value, up_valor.value)
+    limpar()
+    getVenda(up_id.value)
+    up_id.value= ''
+    up_coluna.value= ''
+    up_valor.value= ''
+    
+})
+
+bt_deleta.addEventListener('click', ()=>{
+    deletaVenda(del_id.value)
+    del_id.value= ''
+    limpar()
     getVendas()
 })
